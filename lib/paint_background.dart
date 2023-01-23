3592df3d4cc6d166d56f2572bf14dbcf102c42ea
Clare Heinbaugh <clare.heinbaugh@gmail.com>
2023-01-22 23:02:33 -0500

    Resizing screen moves bucket

--- lib/paint_background.dart
+++ lib/paint_background.dart
@@ -2,15 +2,12 @@ import 'package:flame/components.dart';
 import 'dart:ui';
 
 class PaintWorld extends SpriteComponent with HasGameRef {
+  PaintWorld() : super(size: Vector2.array([window.physicalSize.width, window.physicalSize.height]));
+
   @override
   Future<void> onLoad() async {
     super.onLoad();
     sprite = await gameRef.loadSprite('background.png');
-
-    var physicalScreenSize = window.physicalSize;
-    var physicalWidth = physicalScreenSize.width;
-    var physicalHeight = physicalScreenSize.height;
-
-    size = Vector2.array([physicalWidth, physicalHeight]);
+    size = sprite!.originalSize;
   }
 }
--- lib/paint_bucket.dart
+++ lib/paint_bucket.dart
@@ -1,12 +1,17 @@
 import 'package:flame/components.dart';
 
 class PaintBucket extends SpriteComponent with HasGameRef{
-  PaintBucket() : super(size: Vector2.all(100.0));
+  PaintBucket() : super(size: Vector2.all(100.0), anchor: Anchor.center);
 
   @override
   Future<void> onLoad() async {
     super.onLoad();
     sprite = await gameRef.loadSprite('red_bucket.png');
-    position = gameRef.size / 3;
+  }
+
+  @override
+  void onGameResize(Vector2 gameSize) {
+    super.onGameResize(gameSize);
+    position = Vector2(gameSize.x / 2, gameSize.y - 70.0);
   }
 }
